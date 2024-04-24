a9dfecf9935792dbf58e9ff6d3d449676909e0fb
Jvbass <juan.vpinoc@gmail.com>
2024-04-24 11:20:43 -0400

    Agrega LoginActivity y su layout

---
+++ app/src/main/java/cl/jpinodev/virtualwallet/LogInActivity.java
@@ -0,0 +1,14 @@
+package cl.jpinodev.virtualwallet;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+
+    }
+}
