3b609ab04842a0049523063c620ffd0a5251a9c3
Jvbass <juan.vpinoc@gmail.com>
2024-04-24 00:36:38 -0400

    Agrega MainActivity instanciando splash_screen

--- app/src/main/java/cl/jpinodev/virtualwallet/MainActivity.java
+++ app/src/main/java/cl/jpinodev/virtualwallet/MainActivity.java
@@ -1,19 +1,25 @@
 package cl.jpinodev.virtualwallet;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int SPLASH_DURATION = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.splash_screen);
+        // Utiliza un Handler para postergar la apertura de la siguiente actividad
+        new Handler().postDelayed(() -> {
+            // Este método se ejecutará después del tiempo especificado
+            Intent intent = new Intent(MainActivity.this, LoginSignupActivity.class);
+            startActivity(intent);
+            finish(); // Cierra esta actividad para prevenir regresar a ella con el botón de atrás
+        }, SPLASH_DURATION);
     }
 }
