81ba6055460151abfe5176037dd762ce3d987db7
Jvbass <juan.vpinoc@gmail.com>
2024-04-28 19:08:51 -0400

    Agrega fragmento header y activity home de user.

--- app/src/main/java/cl/jpinodev/virtualwallet/LogInActivity.java
+++ app/src/main/java/cl/jpinodev/virtualwallet/LogInActivity.java
@@ -1,14 +1,24 @@
 package cl.jpinodev.virtualwallet;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LogInActivity extends AppCompatActivity {
 
+    Button loginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        loginBtn = findViewById(R.id.login_button); //se obtiene la referencia del botón
+        loginBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(LogInActivity.this, HomePage.class);
+            startActivity(intent);
+        });
 
     }
 }
