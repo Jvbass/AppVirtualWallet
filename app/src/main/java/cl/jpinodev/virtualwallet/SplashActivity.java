2182ff0d983b09164516c75466e3063c00971d30
Jvbass <juan.vpinoc@gmail.com>
2024-04-28 13:10:24 -0400

    Comentarios a splash activity, modifica tiempo de carga a 2s

--- app/src/main/java/cl/jpinodev/virtualwallet/SplashActivity.java
+++ app/src/main/java/cl/jpinodev/virtualwallet/SplashActivity.java
@@ -7,17 +7,17 @@ import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
-    private static final int SPLASH_DURATION = 3000;
+    private static final int SPLASH_DURATION = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash_screen);
+        setContentView(R.layout.activity_splash_screen);
         // Utiliza un Handler para postergar la apertura de la siguiente actividad
         new Handler().postDelayed(() -> {
-            // Este método se ejecutará después del tiempo especificado
-            Intent intent = new Intent(MainActivity.this, LoginSignupActivity.class);
+            //método se ejecutará después del tiempo especificado
+            Intent intent = new Intent(SplashActivity.this, LoginSignupActivity.class);
             startActivity(intent);
             finish(); // Cierra esta actividad para prevenir regresar a ella con el botón de atrás
         }, SPLASH_DURATION);
