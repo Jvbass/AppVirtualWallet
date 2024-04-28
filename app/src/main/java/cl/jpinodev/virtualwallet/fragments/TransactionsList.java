a3f941306b9c90b96e482f9cc5ace59610a9c1cd
Jvbass <juan.vpinoc@gmail.com>
2024-04-28 19:08:11 -0400

    Agrega fragmentos de transacciones vacia y botones del home user

---
+++ app/src/main/java/cl/jpinodev/virtualwallet/fragments/TransactionsList.java
@@ -0,0 +1,29 @@
+package cl.jpinodev.virtualwallet.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import cl.jpinodev.virtualwallet.R;
+
+public class TransactionsList extends Fragment {
+
+    public TransactionsList() {
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_transactions_list, container, false);
+        return view;
+    }
+}
