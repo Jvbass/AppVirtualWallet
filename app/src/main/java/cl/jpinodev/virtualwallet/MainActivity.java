588274f8071e92ff6a0465e80876284a78867b3a
Jvbass <juan.vpinoc@gmail.com>
2024-04-29 10:49:59 -0400

    implementa onViewCreated en SignUpFrgmntNav

--- app/src/main/java/cl/jpinodev/virtualwallet/MainActivity.java
+++ app/src/main/java/cl/jpinodev/virtualwallet/MainActivity.java
@@ -15,7 +15,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
     }
 }
--- app/src/main/java/cl/jpinodev/virtualwallet/fragments/SignUpFrgmntNav.java
+++ app/src/main/java/cl/jpinodev/virtualwallet/fragments/SignUpFrgmntNav.java
@@ -2,11 +2,16 @@ package cl.jpinodev.virtualwallet.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import cl.jpinodev.virtualwallet.R;
 
@@ -27,5 +32,19 @@ public class SignUpFrgmntNav extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_sign_up_frgmnt_nav, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        Button crearCuentaBtn = view.findViewById(R.id.crearCuentaBtn);
+        TextView loginLink = view.findViewById(R.id.linkCrearCuenta);
+
+        crearCuentaBtn.setOnClickListener(v -> {
+            Navigation.findNavController(v).navigate(R.id.signUpFrgmntNav);
+        });
+
+        loginLink.setOnClickListener(v -> {
+            Navigation.findNavController(v).navigate(R.id.loginFrgmntNav);
+        });
     }
 }
