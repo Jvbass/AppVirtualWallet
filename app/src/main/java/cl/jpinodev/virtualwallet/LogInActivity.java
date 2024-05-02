1b1885f86cbd9bf3687ff07add840a97adfa2ddf
Jvbass <juan.vpinoc@gmail.com>
2024-05-01 22:05:26 -0400

    Enlace desde LogInActivity a pagina transacciones

--- app/src/main/java/cl/jpinodev/virtualwallet/LogInActivity.java
+++ app/src/main/java/cl/jpinodev/virtualwallet/LogInActivity.java
@@ -3,12 +3,14 @@ package cl.jpinodev.virtualwallet;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LogInActivity extends AppCompatActivity {
 
     Button loginBtn;
+    TextView crearCuentaLink;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,9 +18,16 @@ public class LogInActivity extends AppCompatActivity {
 
         loginBtn = findViewById(R.id.login_button); //se obtiene la referencia del botón
         loginBtn.setOnClickListener(v -> {
-            Intent intent = new Intent(LogInActivity.this, HomePage.class);
+            Intent intent = new Intent(LogInActivity.this, HomePageActivity.class);
             startActivity(intent);
         });
+
+        crearCuentaLink = findViewById(R.id.link_crear_cuenta); //se obtiene la referencia del link
+        crearCuentaLink.setOnClickListener(v -> {
+            Intent intent = new Intent(LogInActivity.this, SignUpActivity.class);
+            startActivity(intent);
+        });
+
 
     }
 }
