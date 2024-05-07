4a81b992fb258b9ffcbf9971727e984a586041de
Juan V. Pino Contreras <juan.vpinoc@gmail.com>
2024-05-07 12:14:05 -0400

    Agrega funcionalidad navegacion

--- app/src/main/java/cl/jpinodev/virtualwallet/SignUpActivity.java
+++ app/src/main/java/cl/jpinodev/virtualwallet/SignUpActivity.java
@@ -2,11 +2,13 @@ package cl.jpinodev.virtualwallet;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 public class SignUpActivity extends AppCompatActivity {
     TextView linkLogin;
+    Button crearCuentaBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +16,13 @@ public class SignUpActivity extends AppCompatActivity {
 
         linkLogin = findViewById(R.id.linkLogin);
         linkLogin.setOnClickListener(v -> {
+            Intent intent = new Intent(SignUpActivity.this, LogInActivity.class);
+            startActivity(intent);
+        });
+
+
+        crearCuentaBtn = findViewById(R.id.crearCuentaBtn);
+        crearCuentaBtn.setOnClickListener(v -> {
             Intent intent = new Intent(SignUpActivity.this, LogInActivity.class);
             startActivity(intent);
         });
