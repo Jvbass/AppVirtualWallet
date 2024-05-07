f9982375ca2ea338e4d414e60235128e86314220
Juan V. Pino Contreras <juan.vpinoc@gmail.com>
2024-05-07 12:11:19 -0400

    Agrega agrega fragmento botton al head de home page

--- app/src/main/java/cl/jpinodev/virtualwallet/HomePageActivity.java
+++ app/src/main/java/cl/jpinodev/virtualwallet/HomePageActivity.java
@@ -1,37 +1,54 @@
 package cl.jpinodev.virtualwallet;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
-import cl.jpinodev.virtualwallet.fragments.ButtonsHomeUser;
+import cl.jpinodev.virtualwallet.fragments.EmptyTransactionList;
 import cl.jpinodev.virtualwallet.fragments.TransactionList;
 import cl.jpinodev.virtualwallet.fragments.UserHeader;
 
 public class HomePageActivity extends AppCompatActivity {
 
+    public static boolean fragmentTransactionVisible = false;
+    public static boolean fragmentSenderVisible = false;
+    public static boolean fragmenReceiverVisible = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-        
         loadHeaderUserFragment();
-        loadButtonsHomeUserFragment();
         loadHomeUserContentFragment();
     }
-
-    FragmentManager fm = getSupportFragmentManager();
+    FragmentManager fm = getSupportFragmentManager();// FragmentManager se encarga de la interacción con los fragmentos
 
     private void loadHeaderUserFragment() {
         fm.beginTransaction().replace(R.id.fragmentUserHeader, new UserHeader()).commit();
     }
-    private void loadButtonsHomeUserFragment() {
-        fm.beginTransaction().replace(R.id.buttonPanel, new ButtonsHomeUser()).commit();
-    }
 
     private void loadHomeUserContentFragment() {
-        fm.beginTransaction().replace(R.id.fragmentUserContent, new TransactionList()).commit();
+        fm.beginTransaction().replace(R.id.fragmentEmptyTransaction, new EmptyTransactionList()).commit();
     }
 
+    /*
+    * metodo cambia visivilidad del framelayout cuando escuche click en el boton de notificaciones en el fragmento header
+    * */
+    public void toggleTransactionVisibility() {
+        if (!fragmentTransactionVisible) {
+            findViewById(R.id.fragmentTransaction).setVisibility(View.VISIBLE);
+            fragmentTransactionVisible = true;
+            fm.beginTransaction().replace(R.id.fragmentTransaction, new TransactionList()).commit();
+        } else {
+            findViewById(R.id.fragmentTransaction).setVisibility(View.GONE);
+            fragmentTransactionVisible = false;
+        }
+    }
 }
