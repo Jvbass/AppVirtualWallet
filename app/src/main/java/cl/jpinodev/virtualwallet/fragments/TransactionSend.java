69d6e52c9a74d78e8019ae654293f7efcf2bd4e3
Juan V. Pino Contreras <juan.vpinoc@gmail.com>
2024-05-07 14:59:44 -0400

    Agrega metodo onclick para materialToolBar

--- app/src/main/java/cl/jpinodev/virtualwallet/fragments/TransactionSend.java
+++ app/src/main/java/cl/jpinodev/virtualwallet/fragments/TransactionSend.java
@@ -1,32 +1,32 @@
 package cl.jpinodev.virtualwallet.fragments;
-
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.appbar.MaterialToolbar;
+
+import cl.jpinodev.virtualwallet.HomePageActivity;
 import cl.jpinodev.virtualwallet.R;
-
-
 public class TransactionSend extends Fragment {
     public TransactionSend() {
         // Required empty public constructor
     }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_transaction_send, container, false);
+        View  view = inflater.inflate(R.layout.fragment_transaction_send, container, false);
+
+        MaterialToolbar toolbar = view.findViewById(R.id.materialToolbar);
+
+        toolbar.setNavigationOnClickListener(v -> {
+            Intent intent = new Intent(getContext(), HomePageActivity.class);
+            startActivity(intent);
+            HomePageActivity.fragmenReceiverVisible = true;
+        });
+        return view;
     }
 }
